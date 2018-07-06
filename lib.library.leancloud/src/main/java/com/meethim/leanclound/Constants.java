711afe1af7508604ee733ff772b2b317dcf95fa3
Wuzhao <wuzhao@in66.com>
2018-07-06 17:45:06 +0800

    Feature: square

---
+++ lib.library.leancloud/src/main/java/com/meethim/leanclound/Constants.java
@@ -0,0 +1,13 @@
+package com.meethim.leanclound;
+
+/**
+ * $desc$
+ *
+ * @author wuzhao
+ * @Date 2018/6/28
+ * @mail： wuzhao@in66.com
+ */
+public class Constants {
+    public static final String LEAN_CLOUD_APPID = "tMuiwjuOcUX3qpekVWNQxfHU-gzGzoHsz";
+    public static final String LEAN_CLOUD_CLIENT_KEY = "urkcutrlOzv9zGQYYDzfwUSN";
+}
